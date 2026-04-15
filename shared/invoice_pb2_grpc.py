16e6b18022dbac9143e319e66aef74f6738b9474
KairoHaka <eugen.larche@gmail.com>
2026-04-15 10:52:16 +0200

    Getrennt+ImportsFertig

--- shared/invoice_pb2_grpc.py
+++ shared/invoice_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import invoice_pb2 as invoice__pb2
+from shared import invoice_pb2 as invoice__pb2
 
 GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
