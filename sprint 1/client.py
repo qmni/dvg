4b988a7e981264f1887983ccb2cf6d09b3f7f37b
Minh Nguyen <qminhnguyen99@gmail.com>
2026-05-29 02:27:58 +0200

    fixed issues

--- sprint 1/client.py
+++ sprint 1/client.py
@@ -1,8 +1,8 @@
 import pika
 import json
 import grpc
-from shared import invoice_pb2
-from shared import invoice_pb2_grpc
+import invoice_pb2
+import invoice_pb2_grpc
 from dotenv import load_dotenv
 import os
 
