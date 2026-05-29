7e15b5c564616e2c2f4abee7fcc48dacce0684a0
qmni <qminhnguyen99@gmail.com>
2026-05-29 02:05:18 +0200

    delete fastapi invoice

--- sprint 4/invoice-service/app/models/check_result.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class CheckResult(BaseModel):
-    validation_passed: bool
-    compliance_passed: bool
-    requires_manager_approval: bool
-    errors: List[str] = []
-    warnings: List[str] = []
